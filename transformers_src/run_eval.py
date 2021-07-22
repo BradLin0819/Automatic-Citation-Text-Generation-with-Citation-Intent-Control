e98ecd6f25b7f6a7f0ec4858e557d561af553485
BradLin0819 <brad19970819@gmail.com>
2021-07-22 08:51:02 +0000

    Modified inference script

--- transformers_src/run_eval.py
+++ transformers_src/run_eval.py
@@ -197,6 +197,4 @@ def run_generate(verbose=True):
 
 
 if __name__ == "__main__":
-    # Usage for MT:
-    # python run_eval.py MODEL_NAME $DATA_DIR/test.source $save_dir/test_translations.txt --reference_path $DATA_DIR/test.target --score_path $save_dir/test_bleu.json  --task translation $@
     run_generate(verbose=True)
